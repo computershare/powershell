849e32721f4c4ac09f64dbe36e00678d17346f2f
Mike Jones <github@smenzo.id.au>
2016-05-28 18:18:59 +1000

    unzipPS40.ps1 for Server 2012 R2

---
+++ unzipPS40.ps1
@@ -0,0 +1,10 @@
+<#
+Windows Server 2012 R2 has PowerShell 4.0 by default 
+PowerShell 5.0 has native Expand-Archive cmdlet to unzip files
+This script will unzip a file in PowerShell 4.0 without the need to update the server to PowerShell 5 which would require a reboot
+#>
+
+$shellApplication = new-object -com shell.application
+$zipPackage = $shellApplication.NameSpace($args[0])
+$destinationFolder = $shellApplication.NameSpace($args[1])
+$destinationFolder.CopyHere($zipPackage.Items())
