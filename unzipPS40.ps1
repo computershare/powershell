9168213b97bc777751ec5c0bb9acb6c19e9af175
Mike Jones <github@smenzo.id.au>
2016-05-28 20:33:26 +1000

    Hardcoded paths to test parameters
    Getting errors executing this script on Azure, cannot debug as there is no help or debug info so trying hardcoded values.

--- unzipPS40.ps1
+++ unzipPS40.ps1
@@ -3,9 +3,11 @@ Windows Server 2012 R2 has PowerShell 4.0 by default
 PowerShell 5.0 has native Expand-Archive cmdlet to unzip files
 This script will unzip a file in PowerShell 4.0 without the need to update the server to PowerShell 5 which would require a reboot
 #>
+$zipFile = "d:\sonarqube.zip"
+$destPath = "c:\sonarqube"
 
 $shellApplication = new-object -com shell.application
-$zipPackage = $shellApplication.NameSpace($args[0])
-New-Item -Path $args[1] -ItemType directory -Force
-$destinationFolder = $shellApplication.NameSpace($args[1])
+$zipPackage = $shellApplication.NameSpace($zipFile)
+New-Item -Path $destPath -ItemType directory -Force
+$destinationFolder = $shellApplication.NameSpace($destPath)
 $destinationFolder.CopyHere($zipPackage.Items())
