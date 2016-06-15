1052c76952692619027def92b7aa1d4c124ceb8d
Mike Jones <github@smenzo.id.au>
2016-06-15 22:33:13 +1000

    Replaced chocolatey install command
    following install (copied from chocolatey install page) did not work on out of the box Windows 7 machine
    iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex

--- InstallPowerShell5.ps1
+++ InstallPowerShell5.ps1
@@ -1,3 +1,3 @@
 # Get-ExecutionPolicy must be at least RemoteSigned
-iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex
+iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
 choco install powershell -y
