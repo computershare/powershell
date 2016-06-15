9a9b38d4b7d351cdaffb101d39dd2e742088a39e
Mike Jones <github@smenzo.id.au>
2016-06-15 22:37:38 +1000

    Update InstallPowerShell5.ps1

--- InstallPowerShell5.ps1
+++ InstallPowerShell5.ps1
@@ -1,3 +1,4 @@
 # Get-ExecutionPolicy must be at least RemoteSigned
 iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
 choco install powershell -y
+# Restart-Computer # for the new version to take effect!
