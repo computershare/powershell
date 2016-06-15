df91f4ba11466947d331992388563aa8f7088d0d
Mike Jones <github@smenzo.id.au>
2016-06-15 13:39:24 +1000

    Update utilityPackages.ps1
    Added comments

--- utilityPackages.ps1
+++ utilityPackages.ps1
@@ -1,13 +1,14 @@
 ﻿iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
 
-
+# PowerShell 5 for DSC support
 choco install powershell -pre -y
 
-
 # Free open source tool for editing text and script files with syntax highlighting (better than Windows Notepad)
+# Note SOE developer image already contains UltraEdit
 choco install notepadplusplus -y
 
 # Free open source tool for taking screenshots and annotate them (better than Windows Snipping Tool)
+# Note SOE developer image already contains Greenshot
 choco install greenshot -y
 
 # Free open source tool for comparing files (useful for comparing results from proxy data tool)
