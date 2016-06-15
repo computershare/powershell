33e1f03e3737865cf822e9060f65bbbfd68506da
Mike Jones <github@smenzo.id.au>
2016-06-15 21:11:57 +1000

    Adding DSC utility packages

---
+++ dsc/dscUtilityPackages.ps1
@@ -0,0 +1,40 @@
+Configuration SonarQube 
+{ 
+
+    Import-DscResource -ModuleName cChoco 
+
+    Node "localhost" {   
+
+        cChocoInstaller installChoco
+        { 
+            InstallDir = "C:\ProgramData\chocolatey" 
+        }
+
+        <# Free open source tool for editing text and script files with syntax highlighting (better than Windows Notepad) 
+         # Note SOE developer image already contains UltraEdit #>
+        cChocoPackageInstaller greenshot 
+        { 
+            Name = "notepadplusplus" 
+            DependsOn = "[cChocoInstaller]installChoco" 
+        } 
+
+        <# Free open source tool for taking screenshots and annotate them (better than Windows Snipping Tool) 
+         # Note SOE developer image already contains Greenshot #>
+        cChocoPackageInstaller greenshot 
+        { 
+            Name = "greenshot" 
+            DependsOn = "[cChocoInstaller]installChoco" 
+        } 
+
+        <# Free open source tool for comparing files (useful for comparing results from proxy data tool) #>
+        cChocoPackageInstaller kdiff 
+        { 
+            Name = "kdiff3" 
+            DependsOn = "[cChocoInstaller]installChoco" 
+        } 
+
+    }
+}
+
+SonarQube
+Start-DscConfiguration SonarQube -Wait -Verbose
